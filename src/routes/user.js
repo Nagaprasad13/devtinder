17a26e7ed616ff3668273c4f0efc9309874a7c01
Nagaprasad13 <nagaprasad.013.03@gmail.com>
2026-09-07 14:46:52 +0530

    made feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express=require('express');
 const {requestConnectionModel}=require('../models/connectionRequest');
 const userRouter=express.Router();
 const {userAuth}=require('../middlewares/auth');
+const { User } = require('../models/user');
 userRouter.get('/user/connection',userAuth,async(req,res)=>{
     try{
         const user=req.user;
@@ -36,5 +37,33 @@ userRouter.get('/user/request',userAuth,async(req,res)=>{
     catch(err){
         res.status(401).send('ERROR:'+err.message);
     }
+});
+userRouter.get('/user/feed',userAuth,async(req,res)=>{
+    try{
+    const user=req.user;
+    const page=parseInt(req.query.page)||1;
+    const limit=parseInt(req.query.limit)||5;
+    limit=limit>13?13:limit;
+    const skip=(page-1)*limit;
+    const connections=await requestConnectionModel.find({
+        $or:[{fromUserId:user._id},{toUserId:user._id}],
+    });
+    const excluded=connections.map((connection)=>{
+        if(connection.toUserId.equals(user._id)){
+            return connection.fromUserId;
+        }
+        return connection.fromUserId;
+    });
+    excluded.push(user._id);
+    const feed=await User.find({    
+        _id:{
+            $nin:excluded
+        }
+    }).select("firstName lastName").skip(skip).limit(limit);
+    res.status(200).json({message:"feed is fetched",data:feed});
+}
+catch(err){
+    res.status(200).json({message:err.message});
+}
 })
 module.exports={userRouter};
